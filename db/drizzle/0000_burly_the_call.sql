70ee509d61e5b699ccce25e2fd236000a30c8e0e
Savin Angel-Mario <hello@m4.rocks>
2026-04-19 22:48:58 +0300

    feat: add database schema for account, jwks, projects, servers, session, and user tables

--- db/drizzle/0000_burly_the_call.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `projects` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`slug` text NOT NULL
-);
--- packages/web/src/lib/auth.ts
+++ packages/web/src/lib/auth.ts
@@ -1,9 +1,13 @@
 import { GH_CLIENT_ID, GH_CLIENT_SECRET } from "astro:env/server";
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
+import { betterAuth } from "better-auth";
+import { jwt } from "better-auth/plugins";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
+	plugins: [
+		jwt()
+	],
 	database: drizzleAdapter(db, {
 		provider: "sqlite",
 	}),
