5790c1d7e415015e6125c1475172a1fc49360747
Savin Angel-Mario <hello@m4.rocks>
2026-04-20 00:03:04 +0300

    feat: add production deployment workflow and remote database migration script

---
+++ db/drizzle.remote.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	dialect: "turso",
+	schema: "./db/schema.ts",
+	out: "./db/drizzle",
+
+	dbCredentials: {
+		// biome-ignore lint/style/noNonNullAssertion: Database credentials always present 
+		url: import.meta.env.DATABASE_URL!,
+		// biome-ignore lint/style/noNonNullAssertion: Database credentials always present
+		authToken: import.meta.env.DATABASE_TOKEN!
+	},
+});
